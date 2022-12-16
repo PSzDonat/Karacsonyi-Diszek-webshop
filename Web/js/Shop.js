48da61e6fce5272b7e6d9004a028f63719601349
garobcsi <gasparrobert0908@gmail.com>
2022-12-16 12:16:46 +0100

    store

--- Web/js/Shop.js
+++ Web/js/Shop.js
@@ -4,11 +4,11 @@ async function getFetch(name) {
 }
 function createCard(id,category,images,h5,price) {
     const col = document.createElement('div');
-    col.classList.add('col-xl-3','col-md-4','col-sm-6');
+    col.classList.add('col-xl-3','col-md-4','col-sm-6','mb-4');
     const a = document.createElement('a');
     a.href = `./Termek.html?id=`+id;
     const card = document.createElement('card');
-    card.classList.add('card');
+    card.classList.add('card','h-100');
     const img = document.createElement('img');
     img.src = '../Img/'+category+'/'+images
     const card_body = document.createElement('card-body');
@@ -29,9 +29,7 @@ function createCard(id,category,images,h5,price) {
     col.append(a);
     return col;
 }
-async function showCards() {
-    let datas = [];
-    await getFetch('Datas').then(x=>x.json()).then(x=>datas=x);
+async function showCards(datas) {
     for (const item of datas) {
         let row = document.querySelector(".container>.row");
         row.append(createCard(
@@ -39,5 +37,45 @@ async function showCards() {
         ));
     }
 }
-showCards();
+makeNavigation(12);
+async function makeNavigation(size) {
+    const params = new URLSearchParams(location.search);
+    let datas = [];
+    await getFetch('Datas').then(x=>x.json()).then(x=>datas=x);
+    const currentPage = params.has("page") ? params.get("page") : 1;
+    const calcPageSize= Math.ceil(datas.length/size);
+    const calcA = Math.max(currentPage-1,1);
+    const calcB = Math.min(currentPage-(-1),calcPageSize);
 
+    const navig = document.querySelector(".navig");
+
+    const previous = document.createElement("li");
+    previous.classList.add("page-item");
+    if (currentPage == 1) previous.classList.add("disabled");
+    const previousA = document.createElement("a");
+    previousA.classList.add("page-link");
+    previousA.textContent ="Previous";
+    previousA.href = "Shop.html?page="+(currentPage-1);
+    previous.append(previousA);
+    navig.append(previous);
+    for(let i = calcA; i<=calcB;i++) {
+        const li = document.createElement("li");
+        li.classList.add("page-item");
+        const a = document.createElement("a");
+        a.classList.add("page-link");
+        a.href = "Shop.html?page="+i;
+        a.textContent = i;
+        li.append(a);
+        navig.append(li);
+    }
+    const next = document.createElement("li");
+    next.classList.add("page-item");
+    if (currentPage == calcPageSize) next.classList.add("disabled");
+    const nextA = document.createElement("a");
+    nextA.classList.add("page-link");
+    nextA.textContent ="Next";
+    nextA.href = "Shop.html?page="+(currentPage-(-1));
+    next.append(nextA);
+    navig.append(next);
+    showCards(datas.slice((currentPage - 1) * size,currentPage*size));
+}
